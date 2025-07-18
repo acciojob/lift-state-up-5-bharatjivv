b9633d1318c750563ab125a3474aa4e58500e75a
BharatTate <hibharatj@gmail.com>
2025-07-18 12:42:12 +0530

    addedheading

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
   const handleLogin = () => {
+    console.log(isLoggedIn);
     setIsLoggedIn(true);
   }
   
   return (
     <div>
+    <h1>Parent Component </h1>
         {/* Do not remove the main div */}
         {isLoggedIn ? (
           <h1>Welcome user, Logged in!</h1>
